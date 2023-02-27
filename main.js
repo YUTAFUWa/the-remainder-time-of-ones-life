1ab4e3c19033f3d72873e34e7f15a72aac07b4f8
okada-kenichi <okada.kenichi@gmail.com>
2023-02-27 22:39:32 +0900

    innerHTML

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-let newElm = '';
+let newElm = "";
 
-const elm = document.getElementById('age');
-const answer = document.getElementById('answer');
-const btn = document.getElementById('btn');
-
+const elm = document.getElementById("age");
+const answer = document.getElementById("answer");
+const btn = document.getElementById("btn");
+const averageLife = 84;
 
 // 選べる年齢の選択肢
 const addDom = () => {
-  for (let i = 0; i <= 100; i++) {
-    if ( i == 50) {
-      newElm += '<option value="0" selected> 選んでください▼ </option>\n';
+    for (let i = 0; i <= 100; i++) {
+        if (i == 50) {
+            newElm += '<option value="0" selected> 選んでください▼ </option>\n';
+        }
+        newElm += `<option value=${i}> ${i}歳▼ </option>\n`;
     }
-    newElm += '<option value=${i}> ${i}歳▼ </option>\n';
-  }
-  elm.innerHTML = newElm;
+    elm.innerHTML = newElm;
 };
 addDom();
-
 
 // 選択した年齢の情報を取得
 
@@ -24,20 +23,21 @@ addDom();
 // displayをnoneで作成。
 // inlineもしくはblockに変更すればいい？
 
-
-
-// ボタンが押下された時に結果を表示する
-btn.addEventListener('click',() => {})
-
-
-
-
-
-// 平均寿命から選択した年齢を引き算して残りの寿命を出す
-const cal = () => {
-  
+function getValue() {
+    const selectAge = elm.value;
+    const remainingLife = averageLife - selectAge;
+    const remHour = 365.25 * 24 * remainingLife;
+    if (selectAge >= averageLife) {
+        answer.innerHTML = "長生きしてください";
+    } else {
+        answer.innerHTML = remHour + "時間です";
+    }
 }
 
+// ボタンが押下された時に結果を表示する
+btn.addEventListener("click", () => {
+    getValue();
+});
 
-
-
+// 平均寿命から選択した年齢を引き算して残りの寿命を出す
+const cal = () => {};
