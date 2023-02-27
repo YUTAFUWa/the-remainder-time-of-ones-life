cfae3052727c7431af71d94cbb5ea06a2d46529b
YUTAFUWa <pokopokkopoko@gmail.com>
2023-02-27 22:52:03 +0900

    main

--- main.js
+++ main.js
@@ -1,33 +1,36 @@
-let newElm = '';
+let newAge = '';
 
-const elm = document.getElementById('age');
+const age = document.getElementById('age');
 const answer = document.getElementById('answer');
 const btn = document.getElementById('btn');
-
+const agePlace = document.getElementById('agePlace');
 
 // 選べる年齢の選択肢
 const addDom = () => {
   for (let i = 0; i <= 100; i++) {
     if ( i == 50) {
-      newElm += '<option value="0" selected> 選んでください▼ </option>\n';
+      newAge += `<option value="0" selected> 選んでください▼ </option>\n`;
     }
-    newElm += '<option value=${i}> ${i}歳▼ </option>\n';
+    newAge += `<option value=${i}> ${i}歳▼ </option>\n`;
   }
-  elm.innerHTML = newElm;
+  age.innerHTML = newAge;
 };
 addDom();
 
 
 // 選択した年齢の情報を取得
+function getAge(){
+  console.log(age.value);
+};
 
 // 結果はHTMLで、
 // displayをnoneで作成。
 // inlineもしくはblockに変更すればいい？
 
-
-
 // ボタンが押下された時に結果を表示する
-btn.addEventListener('click',() => {})
+btn.addEventListener('click',() => {
+  getAge();
+})
 
 
 
