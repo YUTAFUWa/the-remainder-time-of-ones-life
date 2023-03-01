f05f50b6883f69ce7aa1c5d2d094c5896b28e380
YUTAFUWa <pokopokkopoko@gmail.com>
2023-03-01 12:28:27 +0900

    add_gender

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const answer = document.getElementById('answer');
 const btn = document.getElementById('btn');
 const agePlace = document.getElementById('agePlace');
 const averageLife = 81.47;
+const gender = document.getElementById('gender');
 
 // 選べる年齢の選択肢
 const addDom = () => {
@@ -19,6 +20,12 @@ const addDom = () => {
 addDom();
 
 
+// ↓↓↓averageLifeを男女ごとに分けてみる↓↓↓
+// if(gender.value = 'men'){
+//   averageLife = 81.47;
+// }
+
+
 // 選択した年齢の情報を取得
 function getAge(){
   const selectAge = age.value;
@@ -30,20 +37,8 @@ function getAge(){
 // 結果はHTMLで、
 // displayをnoneで作成。
 // inlineもしくはblockに変更すればいい？
-
 // ボタンが押下された時に結果を表示する
-btn.addEventListener('click',() => {
-  getAge();
-})
-
-
-
-
-
 // 平均寿命から選択した年齢を引き算して残りの寿命を出す
-const cal = () => {
-  
-}
 
 
 
